2fb86ed05859f46601e08c05f3606b23632a19c0
Mapu456 <mpaula456@hotmail.com>
2021-07-26 20:31:11 -0500

    fix betty issues

--- _printf.c
+++ _printf.c
@@ -3,52 +3,51 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include "holberton.h"
-
+/**
+ * _printf - function
+ * @format: pointer to array
+ * Return: 1.
+ */
 int _printf(const char *format, ...)
 {
-    int i = 0;
-    int p = 0; 
-    int j = 0;
-    char join[1024] = "";
+	int i = 0;
+	int j = 0;
+	char join[1024] = "";
 
-    va_list parametros;
-    va_start (parametros, format);
+	va_list parametros;
 
-    for(i = 0; format[i] != '\0'; i++, j++)
-    {
-        if (format[i] == '%')
-        {
-            if (format[i + 1] == 'c')
-            {
-                join[j] = va_arg(parametros, int);
-                i++;
-            }
-            else if (format[i + 1] == '%')
-            {
-                join[j] = '%';
-                i++;
-            }
-            else if (format[i + 1] == 's')
-            {
-                j = f_strings(va_arg(parametros, char *), join, j);
-                i++;
-            }
-            else
-            {
-                join[j]= format[i];
-            }
-        }
-        else
-        {
-            join[j] = format[i];
-        }
-    }
-    write(1, join, j);
+	va_start(parametros, format);
+
+	for (i = 0; format[i] != '\0'; i++, j++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == 'c')
+			{
+				join[j] = va_arg(parametros, int);
+				i++;
+			}
+			else if (format[i + 1] == '%')
+			{
+				join[j] = '%';
+				i++;
+			}
+			else if (format[i + 1] == 's')
+			{
+				j = f_strings(va_arg(parametros, char *), join, j);
+				i++;
+			}
+			else
+			{
+				join[j] = format[i];
+			}
+		}
+		else
+		{
+			join[j] = format[i];
+		}
+	}
+	write(1, join, j);
+	return (0);
 }
-void main(void)
-{
-    _printf("Char[%%][%c][%c][%c]\n", 'H', 'L', 'H', 'L');
-    _printf("Let's try to printf a %%simple sentence.\n");
-    _printf("Unknown:[%r]\n");
-    _printf("String:[%s]\n", "I am a string !");
-}
+
--- _printf_string.c
+++ _printf_string.c
@@ -4,18 +4,20 @@
 #include <unistd.h>
 #include "holberton.h"
 /**
- * f_strings- function that fills place holders with string 
- * 
+ * f_strings- function that fills place holders with string
+ * @str: takes the parameter of va_arg
+ * @join: is the buffer (takes temporal value)
+ * @j: is the position where will be replace the va_arg
  * Return:j the actual position .
  */
 int f_strings(char *str, char *join, int j)
 {
-    int x = 0;
+int x = 0;
 
-    for (x = 0; str[x] != '\0'; x++)
-    {
-        join[j] = str[x];
-        j++;
-    }
-    return(j);
+for (x = 0; str[x] != '\0'; x++)
+{
+join[j] = str[x];
+j++;
+}
+return (j);
 }
