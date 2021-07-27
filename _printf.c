de5ebfc2b7e5f2e05ee63e010f40a653415da7e8
Laurajcb <callejaslaura96@gmail.com>
2021-07-27 03:38:17 +0000

    Fixed error

--- _printf.c
+++ _printf.c
@@ -3,7 +3,6 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include "holberton.h"
-
 /**
  * _printf - function
  * @format: pointer to array
@@ -11,43 +10,44 @@
  */
 int _printf(const char *format, ...)
 {
-    int i = 0;
-    int j = 0;
-    char join[1024] = "";
+	int i = 0;
+	int j = 0;
+	char join[1024] = "";
 
-    va_list parameters;
-    va_start(parameters, format);
+	va_list parameters;
 
-    for (i = 0; format[i] != '\0'; i++, j++)
-    {
-        if (format[i] == '%')
-        {
-            if (format[i + 1] == 'c')
-            {
-                join[j] = va_arg(parameters, int);
-                i++;
-            }
-            else if (format[i + 1] == '%')
-            {
-                join[j] = '%';
-                i++;
-            }
-            else if (format[i + 1] == 's')
-            {
-                j = f_strings(va_arg(parameters, char *), join, j);
-                i++;
-            }
-            else
-            {
-                join[j] = format[i];
-            }
-        }
-        else
-        {
-            join[j] = format[i];
-        }
-    }
-    va_end(parameters);
-    write(1, join, j);
-    return(j);
+	va_start(parameters, format);
+
+	for (i = 0; format[i] != '\0'; i++, j++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == 'c')
+			{
+				join[j] = va_arg(parameters, int);
+				i++;
+			}
+			else if (format[i + 1] == '%')
+			{
+				join[j] = '%';
+				i++;
+			}
+			else if (format[i + 1] == 's')
+			{
+				j = f_strings(va_arg(parameters, char *), join, j);
+				i++;
+			}
+			else
+			{
+				join[j] = format[i];
+			}
+		}
+		else
+		{
+			join[j] = format[i];
+		}
+	}
+	va_end(parameters);
+	write(1, join, j);
+	return (j);
 }
