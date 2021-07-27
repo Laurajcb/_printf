71c8f0bbd491696880409736952f5c17f3baa371
Mapu456 <mpaula456@hotmail.com>
2021-07-26 22:40:37 -0500

    update files

--- _printf.c
+++ _printf.c
@@ -14,37 +14,40 @@ int _printf(const char *format, ...)
 	int j = 0;
 	char join[1024] = "";
 
-    va_list parameters;
-    va_start (parameters, format);
+	va_list parameters;
 
-    for(i = 0; format[i] != '\0'; i++, j++)
-    {
-        if (format[i] == '%')
-        {
-            if (format[i + 1] == 'c')
-            {
-                join[j] = va_arg(parameters, int);
-                i++;
-            }
-            else if (format[i + 1] == '%')
-            {
-                join[j] = '%';
-                i++;
-            }
-            else if (format[i + 1] == 's')
-            {
-                j = f_strings(va_arg(parameters, char *), join, j);
-                i++;
-            }
-            else
-            {
-                join[j]= format[i];
-            }
-        }
-        else
-        {
-            join[j] = format[i];
-        }
-    }
-    write(1, join, j);
+	va_start(parameters, format);
+
+	for (i = 0; format[i] != '\0'; i++, j++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == 'c')
+			{
+				join[j] = va_arg(parameters, int);
+				i++;
+			}
+			else if (format[i + 1] == '%')
+			{
+				join[j] = '%';
+				i++;
+			}
+			else if (format[i + 1] == 's')
+			{
+				j = f_strings(va_arg(parameters, char *), join, j);
+				i++;
+			}
+			else
+			{
+				join[j] = format[i];
+			}
+		}
+		else
+		{
+			join[j] = format[i];
+		}
+	}
+	va_end(parameters);
+	write(1, join, j);
+	return (j);
 }
